b99b43a05a9e3d4dc511e8303d15589d55753250
MSG8 <masogo008@gmail.com>
2025-12-03 19:02:12 +0100

    fixed/className mal puesto App.js

--- view/src/Componentes/App/App.js
+++ view/src/Componentes/App/App.js
@@ -22,7 +22,7 @@ function App() {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">Navbar</a>
-        <button claclassNamess="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
